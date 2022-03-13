33755aca3b74d7a4685c04641015e6ccf09bb757
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2022-03-13 14:17:31 +0900

    途中段階

--- src/components/charttest.tsx
+++ src/components/charttest.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import 'chart.js/auto';
-import { useRef } from 'react';
+import { ChartData } from 'chart.js/auto';
+import { MouseEvent, useRef } from 'react';
 import { Chart } from 'react-chartjs-2';
 
 const ChartTest = () => {
@@ -9,31 +10,39 @@ const ChartTest = () => {
   for (let i = 0; i < DATA_COUNT; ++i) {
     labels.push(i.toString());
   }
-  const datapoints = [0, 20, 20, 60, 60, 120, 100, 180, 120, 125, 105, 110, 170];
-  const data = {
+  const data01 = [0, 20, 20, 60, 60, 120, 100, 180, 120, 125, 105, 110, 170];
+  const data02 = [0, 30, 10, 20, 20, 80, 20, 300, 280, 240, 140, 80, 60];
+
+  const dataConfig = (label: string, data: number[], color: string) => {
+    return {
+      label: label,
+      data: data,
+      borderColor: color,
+      backgroundColor: color + '20',
+      fill: true,
+      cubicInterpolationMode: 'monotone',
+      tension: 0.4,
+      pointStyle: 'circle',
+      pointRadius: 2,
+      pointHoverRadius: 12,
+    };
+  };
+
+  const data: ChartData = {
     labels: labels,
-    datasets: [
-      {
-        label: 'Cubic interpolation (monotone)',
-        data: datapoints,
-      },
-      {
-        label: 'Cubic interpolation',
-        data: datapoints,
-      },
-      {
-        label: 'Linear interpolation (default)',
-        data: datapoints,
-      },
-    ],
+    datasets: [dataConfig('死亡者数', data01, '#0BC5EA'), dataConfig('重症者数', data02, '#9F7AEA')],
   };
 
   const options = {
     responsive: true,
     plugins: {
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart - Cubic interpolation mode',
+      legend: {
+        labels: {
+          usePointStyle: true,
+        },
+      },
+      tooltip: {
+        // TODO: tooltipを見やすくしたい
       },
     },
     interaction: {
@@ -44,25 +53,32 @@ const ChartTest = () => {
         display: true,
         title: {
           display: true,
+          text: '期間',
         },
       },
       y: {
         display: true,
+        stacked: true,
         title: {
           display: true,
-          text: 'Value',
+          text: '人数',
         },
-        suggestedMin: -10,
-        suggestedMax: 200,
+        ticks: {
+          stepSize: 100,
+        },
       },
     },
   };
 
-  const chartRef = useRef(null);
+  const chartRef = useRef();
+
+  const chartOnClick = (event: MouseEvent) => {
+    console.log('ここで何かしようかな', event);
+  };
 
   return (
     <Box w="full" h="full" bg="white">
-      <Chart ref={chartRef} type="line" data={data} options={options} />
+      <Chart ref={chartRef} type="line" data={data} options={options} onClick={chartOnClick} />
     </Box>
   );
 };
