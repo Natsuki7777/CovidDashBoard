1c3c6f1b690c5dc1571fa9babf6171d0bd4ab6f6
Koshi Osaki <oosaki@futurewoods.co.jpgit config --global user.email oosaki@futurewoods.co.jpgit config --global user.email oosaki@futurewoods.co.jp>
2022-03-18 11:49:13 +0900

    サイドバー、ヘッダー修正等

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,37 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
-import { Box, Flex, Spacer, Text } from '@chakra-ui/react';
+import { Box, Drawer, DrawerContent, DrawerOverlay, Flex, IconButton, Spacer, Text, useDisclosure } from '@chakra-ui/react';
+import DrawerMenu from './drawerMenu';
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Flex as="header" width="full" h={16} alignItems="center" bg="gray.900" px="4">
-      <Flex color="gray.100" h={8} alignItems="center">
-        <HamburgerIcon mr={4} />
-        <Text fontWeight="bold">COVIDVIEW</Text>
+    <>
+      <Flex as="header" width="full" h={16} alignItems="center" bg="gray.900" px="4">
+        <Flex color="gray.100" h={8} alignItems="center">
+          <IconButton
+            _focus={{ outline: 'none' }}
+            mr={3}
+            aria-label="menu"
+            icon={<HamburgerIcon />}
+            size="sm"
+            variant="unstyled"
+            display={{ base: 'block', md: 'none' }}
+            onClick={onOpen}
+          />
+          <Text fontWeight="bold">COVIDVIEW</Text>
+        </Flex>
+        <Spacer />
+        <Box></Box>
       </Flex>
-      <Spacer />
-      <Box></Box>
-    </Flex>
+      <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerMenu />
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 };
 
